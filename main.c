66e3484dee096ecd148940924ac6a2f5596d9577
R H <m.rehamed@gmail.com>
2021-01-25 12:55:50 +0100

    creation fenètre blanche

--- main.c
+++ main.c
@@ -40,9 +40,36 @@ static void printQr(const uint8_t qrcode[]);
 int main(int argc, char  ** argv) {
 	SDL_Window * win = NULL;
     SDL_Renderer * renderer = NULL;
+	int finish  = 0;
 	SDL_Init(SDL_INIT_VIDEO); // TODO error function
 	win  = SDL_CreateWindow("QRcode generator",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WINDOW_WIDTH, WINDOW_HEIGHT,0);//TODO error function
 	renderer = SDL_CreateRenderer(win, -1,SDL_RENDERER_ACCELERATED);
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	while (!finish) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                finish = 1;
+            }
+            switch (event.type) {
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym) {
+                       
+                    }
+                break;
+            }
+			uint8_t qrcode[qrcodegen_BUFFER_LEN_MAX];
+			uint8_t tempBuffer[qrcodegen_BUFFER_LEN_MAX];
+			bool ok = qrcodegen_encodeText("314159265358979323846264338327950288419716939937510", tempBuffer, qrcode,
+				qrcodegen_Ecc_MEDIUM, qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX, qrcodegen_Mask_AUTO, true);
+			if (ok)
+				printQr(qrcode);
+        }
+        //SDL_RenderClear(renderer);
+	}
 	doVarietyDemo();
 	SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(win);
@@ -73,4 +100,20 @@ static void printQr(const uint8_t qrcode[]) {
 		fputs("\n", stdout);
 	}
 	fputs("\n", stdout);
+}
+
+static void printQrSDL(const uint8_t qrcode[]) {
+	int size = qrcodegen_getSize(qrcode);
+	int border = 4;
+	for (int y = -border; y < size + border; y++) {
+		for (int x = -border; x < size + border; x++) {
+			fputs((qrcodegen_getModule(qrcode, x, y) ? "##" : "  "), stdout);
+		}
+		fputs("\n", stdout);
+	}
+	fputs("\n", stdout);
+}
+
+void printDarkRect() {
+	
 }
