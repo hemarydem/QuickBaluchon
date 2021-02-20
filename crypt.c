bef13fbbe57cbe9a996378737981e5a32d4621c9
leo78450 <harpeimangue37@gmail.com>
2021-02-20 03:48:03 +0100

    new algo chiffrement

--- crypt.c
+++ crypt.c
@@ -2,20 +2,54 @@
 
 char * encryptage(char * str, int key)
 {
-    int i;
-    for (i = 0; (i < 255 && str[i] != '\0'); i++) {
-        str[i] = str[i] + key;
+    int i, j, x, y;
+    char test[185] = {"fRSTU#$%&mnLMq[\\]^{45tuva9rbcYZ1de78s()*+,./:;wxyzABCDghijklQEFPNOopGVWX23\"HIJK<=>@0-_!?'|}~fRSTU#$%&mnLMq[\\]^{45tuva9rbcYZ1de78s()*+,./:;wxyzABCDghijklQEFPNOopGVWX23\"HIJK<=>@0-_!?'|}~\0"};
+
+    //fRSTU#$%&mnLMq[\\]^{45tuva9rbcYZ1de78s()*+,./:;wxyzABCDghijklQEFPNOopGVWX23\"HIJK<=>@0-_!?'|}~
+    //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123457890-_!?'\"#$%&()*+,./:;<=>@[\\]^{|}~
+    //
+
+    for(i = 0; (i < 255 && str[i] != '\0'); i++){
+        for(j = 0; j < 91; j++){
+            if(str[i] == test[j]){
+                if(key > 90){
+                    key = key - 89;
+                }
+                printf("\nkey = %d", key);
+                y = j + key;
+                str[i] = test[y];
+                break;
+            }else{
+                continue;
+            }
+        }
     }
+
     printf("\nencrypt str: %s\n", str);
     return str;
 }
 
 char * decryptage(char * str, int key)
 {
-    int i;
+    int i, j, x, y;
+    char test[185] = {"fRSTU#$%&mnLMq[\\]^{45tuva9rbcYZ1de78s()*+,./:;wxyzABCDghijklQEFPNOopGVWX23\"HIJK<=>@0-_!?'|}~fRSTU#$%&mnLMq[\\]^{45tuva9rbcYZ1de78s()*+,./:;wxyzABCDghijklQEFPNOopGVWX23\"HIJK<=>@0-_!?'|}~\0"};
+
     for(i = 0; (i < 255 && str[i] != '\0'); i++){
-        str[i] = str[i] - key;
+        for(j = 92; j < 185; j++){
+            if(str[i] == test[j]){
+                if(key > 90){
+                    key = key - 89;
+                }
+                printf("\nkey = %d", key);
+                y = j - key;
+                str[i] = test[y];
+                break;
+            }else{
+                continue;
+            }
+        }
     }
+
     printf("\ndecrypted str: %s\n", str);
     return str;
 }
