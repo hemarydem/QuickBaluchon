ba884e68f788cde9637a2904c74ae31e7eca0610
R H <m.rehamed@gmail.com>
2021-03-05 13:38:13 +0100

    set db co remy

---
+++ utils/db.php
@@ -0,0 +1,53 @@
+<?php
+
+//'mysql:host=localhost;dbname=sananair;port=8889;charset=utf8', 'jo','r626wst100'
+function getDataBaseConnection(): PDO {
+    //sConnect to a MySQL database using driver invocation
+    $dsn = 'mysql:dbname=qb;host=127.0.0.1';
+    $user = 'remy';
+    $password = 'azerty';
+
+    try {
+       return $dbh = new PDO($dsn, $user, $password);
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+    }
+}
+/*
+
+function dataBaseInsert(PDO $connect, string $sql, array $params): ?string
+{
+    $statement = $connect->prepare($sql);
+    if ($statement !== false) {
+        $success = $statement->execute(params);
+        if ($success) {
+            return $connect->lastInsertId();
+        }
+    }
+    return NULL;
+}
+
+function dataBaseFindOne(PDO $connect, string $sql, array $params): ?array
+{
+    $statement = $connect->prepare($sql);
+    if ($statement !== false) {
+        $success = $statement->execute(params);
+        if ($success) {
+            return $statement->fetch(PDO::FETCH_ASSOC);
+        }
+    }
+    return NULL;
+}
+
+function dataBaseFindAll(PDO $connect, string $sql, array $params): ?array
+{
+    $statement = $connect->prepare($sql);
+    if ($statement !== false) {
+        $success = $statement->execute(params);
+        if ($success) {
+            return $statement->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+    return NULL;
+
+}*/
