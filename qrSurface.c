942988d2d2fbc2e10d8ab1597b52b7e36696fa0a
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-04-05 16:12:08 +0200

    Create qrSurface.c

---
+++ qrSurface.c
@@ -0,0 +1,69 @@
+#include "qrSurface.h"
+
+void qrCodePrintPNG(char * str, char * fileName) { // the main function 
+	printf("start\n");
+	strcat(fileName,".png"); 
+	printf("strcat good\n");
+    SDL_Surface * drawingSheet;
+    int size = 60;
+    int ** array = build2dArray(size);
+    array = Array2dSetToZero(array,size);
+	array = genMapIntegerArray(str); 
+	printf("array good\n");
+	drawingSheet = SDL_CreateRGBSurfaceWithFormat(0, 600, 600, 32, SDL_PIXELFORMAT_RGBA8888); 
+	printf("1\n");
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_LockSurface(drawingSheet);
+	drawingSheet = paintToWhiteSurface(drawingSheet);
+	drawingSheet = paintQrcodeToSurface(drawingSheet, array);
+	IMG_SavePNG(drawingSheet,"qrCode.png");
+	printf("IMG save good\n");
+	rename("qrCode.png", fileName);
+	printf("rename good\n");
+	SDL_UnlockSurface(drawingSheet);
+	printf("2\n");
+	SDL_FreeSurface(drawingSheet);
+	free(array);
+	SDL_Quit();
+	printf("3\n");
+}
+
+SDL_Surface * paintToWhiteSurface(SDL_Surface * drawingSheet) { // set all surface's pixel in white
+	size_t i, j;
+	for(i = 0; i < 600; i++) {				
+        for(j = 0; j < 600; j++) {
+            setPixel(drawingSheet, 0xFF, 0xFF, 0xFF,0xFF, i, j);
+		}
+    }
+	return drawingSheet;
+}
+
+SDL_Surface * paintQrcodeToSurface(SDL_Surface * drawingSheet, int ** array) { // use the 2 dimenssion array [60][60]
+	int m;                                                                     // it draw a black  square each time an array element is set to one
+	int n;
+	size_t xIndixePixel = 0;
+	size_t yIndixePixel = 0;
+	int count = 0;
+	for (m = 0; m < 60; m++) {
+		for(n = 0; n < 60; n++) {
+			if(array[m][n]) {
+				for(size_t q = yIndixePixel; q < 10 + yIndixePixel; q ++) {
+					for(size_t k = xIndixePixel; k < 10 + xIndixePixel; k ++) {
+						setPixel(drawingSheet, 0x0, 0x0, 0x0, 0xFF, k, q);
+					}
+				}
+				xIndixePixel += 10;
+				count++;
+			} else {
+				xIndixePixel += 10;
+				count++;
+			}
+			if(count == 60) {
+				yIndixePixel += 10;
+				count  = 0;
+			}
+		}
+		xIndixePixel = 0;
+	}
+	return drawingSheet;
+}
