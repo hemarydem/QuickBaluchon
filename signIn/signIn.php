20030b81d832abb62a33d7ce24cbc8cb8fcda461
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-05 00:08:44 +0200

    init branch

---
+++ signIn/signIn.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<label>login mail address</label>
+<input type="text" placeholder="mail address" id="mail">
+<label>passwords</label>
+<input type="text" placeholder="password" id="pssword">
+<button onclick="getUser()"></button>
+</body>
+<script src="./script/signIn.js"></script>
+</html>
