639f739493db9350869b21c77850b0b40ae6e2b2
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-10 00:10:29 +0200

    test

--- users/functions/functions.php
+++ users/functions/functions.php
@@ -3,7 +3,7 @@ include ("./../../utils/db.php");
 session_start();
 function insertUser( string $tabName, string $nom, string $prenom, string  $mail, string  $adresse, string $numSiret, string $password, string $tel, int $driverLicence, int $statut, int $busy, int $zoneMaxDef) {
     $db = getDataBaseConnection();
-    $sql = "INSERT INTO user( nom, prenom, mail, adresse, numSiret, password, tel, driverLicence, statut, busy, zoneMaxDef) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+    $sql = "INSERT INTO USER( nom, prenom, mail, adresse, numSiret, password, tel, driverLicence, statut, busy, zoneMaxDef) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
     $params = [ $nom,  $prenom,   $mail,   $adresse,  $numSiret,  $password,  $tel, $driverLicence, $statut, $busy, $zoneMaxDef];
     return dataBaseInsert($db,  $sql, $params, $tabName);
 }
