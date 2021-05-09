ff70daeb7416fe53051410447ea0d4160c1b33ac
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-09 23:45:53 +0200

    ajout ash

--- users/post/creat.php
+++ users/post/creat.php
@@ -26,7 +26,7 @@ insertUser(
     $data->{"mail"},
     $data->{"adresse"},
     $data->{"numSiret"},
-    $data->{"password"},
+    hash('sha256', $data->{"password"}),
     $data->{"tel"},
     $data->{"driverLicence"},
     $data->{"statut"},
