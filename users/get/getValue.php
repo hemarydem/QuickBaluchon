7621e1a1e2257e613ab19cb069f593a0214b5c84
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-10 22:12:30 +0200

    Access-Control

--- users/get/getValue.php
+++ users/get/getValue.php
@@ -1,9 +1,9 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+
 include("./../functions/functions.php");
 include("./../../chckFnctns/chckFnctns.php");
 include("./../../listfnctns/listfnctns.php");
-
+header("Access-Control-Allow-Origin: *");
 checIfsessionStarted();
 if (isset($_GET)) {
     /* if(isset($_GET['tokenApi'])) {
--- users/get/list.php
+++ users/get/list.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 include("./../functions/functions.php");
 include ("./../../chckFnctns/chckFnctns.php");
 include ("./../../listfnctns/listfnctns.php");
