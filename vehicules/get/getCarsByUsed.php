d773018ceb12b1a610937320540328c761194f2e
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-12 01:07:39 +0200

    vehicule maj

---
+++ vehicules/get/getCarsByUsed.php
@@ -0,0 +1,20 @@
+<?php
+include("./../functions/functions.php");
+include ("./../../chckFnctns/chckFnctns.php");
+include ("./../../listfnctns/listfnctns.php");
+header("Access-Control-Allow-Origin: *");
+if(isset($_GET)) {
+    if(isset($_GET["id"])) {
+        $id = intval($_GET["id"]);
+    } else {
+        erroJsonMssg("error id is missing");
+    }
+    $sql = "SELECT VEHICULE.id, VEHICULE.imatriculation, VEHICULE.nbColis, VEHICULE.volumeMax, VEHICULE.weightMax, VEHICULE.employ FROM VEHICULE INNER JOIN OWN ON OWN.idVehicule=VEHICULE.id AND OWN.idUser=? AND employ=1";
+    $rows = execRequestGetALLResults( $sql, [$id]);
+    $json = json_encode($rows);
+    header("Content-Type: application/json");
+    print_r($json);
+} else {
+    http_response_code(500);
+}
+
