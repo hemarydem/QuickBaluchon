16823d50386acd9fc6bbb7cb2068423091d4e6e7
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-12 02:41:47 +0200

    updat function turn on using car

--- home/homeDriver.php
+++ home/homeDriver.php
@@ -70,6 +70,7 @@
             <div class="col-md-2" style="background-color: white">
                 
             </div>
+            <h1>liste des colis à saisir</h1>
             <div class="col-md-6" style="background-color: white" id="COLISLIST">
             
             </div>
