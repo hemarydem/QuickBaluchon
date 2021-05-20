70e20d1528af49bd50eff606afb313f04f295ada
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-20 19:47:36 +0200

    maj crud user

--- users/get/signIn.php
+++ users/get/signIn.php
@@ -4,12 +4,6 @@ include("./../../chckFnctns/chckFnctns.php");
 include("./../../listfnctns/listfnctns.php");
 checIfsessionStarted();
 if (isset($_GET)) {
-    /* if(isset($_GET['tokenApi'])) {
-         chekIfRequestFromShield($_GET['tokenApi']);
-         unset($_GET['tokenApi']);
-     } else {
-         erro400NotConnectJsonMssg( "token api is not set");
-     }*/
     checkStringsArray($_GET,1);
     if(isset($_GET['password']))
         $_GET['password'] = hash("sha256",  $_GET['password']);
@@ -22,10 +16,21 @@ if (isset($_GET)) {
         echo json_encode(["message"=> "no result found"]);
         exit(1);
     }
-    $json = json_encode($rows);
-    header("Access-Control-Allow-Origin: *");
-    header("Content-Type: application/json");
-    print_r($json);
+    if($rows["active"] == 1) {
+        foreach ($rows as $key => $value) {
+            $_SESSION[$key."_session"] = $value;
+        }
+        $json = json_encode($rows);
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json");
+        print_r($json);
+        print_r($_SESSION);
+    } else {
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json");
+        echo json_encode(["message"=> "le compte doit être activé ou à été bloqué"]);
+        exit(1);
+    }
 } else {
     http_response_code(500);
 }
