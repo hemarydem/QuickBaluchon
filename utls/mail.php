1edb5667bb3ebefeb016094e2caf68d84b1cbea8
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-20 17:02:41 +0200

    creation scipt js pour driver

--- utls/mail.php
+++ utls/mail.php
@@ -3,7 +3,7 @@
 $content = file_get_contents('php://input');
 $data = json_decode($content, true);
 
-
+print_r($data);
 if(isset($data["id"]) && isset($data["mail"])){
     $to = $data["mail"];
     $token = base_convert(hash('sha256', time() . mt_rand()), 16, 36);
