7ec2229ccc22bbd85ad0c90f1f53984a7af6bf86
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-20 15:32:57 +0200

    maj crud user

--- users/get/list.php
+++ users/get/list.php
@@ -26,7 +26,7 @@ if(isset($_GET)) {
 //print_r($params);
 //TODO TEST TO ADAPT THE CODE WITH THE FUNCTION BELOW TO MAke the request dynamique
 // $sql = buildsSelectAndattributs($_GET, "user");//listfnctns.php
-    $sql = 'SELECT nom, prenom, mail, adresse, numSiret, password, tel, driverLicence, statut, busy, zoneMaxDef, id FROM USER';
+    $sql = 'SELECT nom, prenom, mail, adresse, numSiret, password, tel, driverLicence, statut, busy, zoneMaxDef, id,tokenEmail, active,licencePath FROM USER';
     if (count($where) > 0) {
         $whereClause = join(" AND ", $where);
         $sql .= " WHERE " . $whereClause;
