bca4152ff799d8cce341426f7c03dafdd92a8b23
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-20 16:59:03 +0200

    creation scipt js pour driver

--- validationMail/script/script.js
+++ validationMail/script/script.js
@@ -57,6 +57,7 @@ function updateActiveUserAtribute() {
     request.onreadystatechange = function() {
         if(request.readyState == 4) {
                 if(request.status == 200) {
+                    console.log(request.responseText);
                     let result = JSON.stringify(request.responseText);
                     if(result.length <= 1) {
                         console.log(result["message"]);
