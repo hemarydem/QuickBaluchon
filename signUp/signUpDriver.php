82fb5beacb52493f8730646967542855879dd0ff
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-21 11:20:52 +0200

    correction script js inscription client succes()

--- signUp/signUpDriver.php
+++ signUp/signUpDriver.php
@@ -19,31 +19,31 @@
             <div class="col-md-4" style="background-color: white" >
                 <div class="col-md-2 col-md-offset-5">
                     <label>NOM</label>
-                    <input type="text" oninput="checkLen(\'name\',50)" placeholder="name" id="name">
+                    <input type="text" oninput="checkLen('name',50)" placeholder="name" id="name">
                     <p id="limitname">50/50</p><p id="erroname"></p>
                 </div>
                 <div class="col-md-2 col-md-offset-5">
                     <label>Prénom</label>
-                    <input type="text" oninput="checkLen(\'firstname\',50)"  placeholder="firstname" id="firstname">
+                    <input type="text" oninput="checkLen('firstname',50)"  placeholder="firstname" id="firstname">
                     <p id="limitfirstname">50/50</p><p id="errofirstname"></p>
                 </div>
             </div>
             <div class="col-md-4" style="background-color: white">
                 <div class="col-md-2 col-md-offset-5">
                     <label>passwords</label>
-                    <input type="text" oninput="checkLen(\'pssword\',255)" value="azerty" placeholder="password" id="pssword">
+                    <input type="text" oninput="checkLen('pssword',255)" value="azerty" placeholder="password" id="pssword">
                     <p id="limitpssword">255/255</p><p id="erropssword"></p>
                 </div>
                 <div class="col-md-2 col-md-offset-5">
                     <label>confirmPassword</label>
-                    <input type="text" oninput="checkLen(\'confiamtionPword\',255)" value="azerty" placeholder="confirme password" id="confiamtionPword">
+                    <input type="text" oninput="checkLen('confiamtionPword',255)" value="azerty" placeholder="confirme password" id="confiamtionPword">
                     <p id="limitconfiamtionPword">255/255</p><p id="erroconfiamtionPword"></p>
                 </div>
             </div>
             <div class="col-md-4" style="background-color: white">
                 <div class="col-md-2 col-md-offset-5">
                     <label>adresse</label>
-                    <input type="text" oninput="checkLen(\'address\',255)" placeholder="address" id="address">
+                    <input type="text" oninput="checkLen('address',255)" placeholder="address" id="address">
                     <p id="limitaddress">255/255</p><p id="erroaddress"></p>
                 </div>
                 <div class="col-md-2 col-md-offset-5">
@@ -57,7 +57,7 @@
             <div class="col-md-4" style="background-color: white">
                 <div class="col-md-2 col-md-offset-5">
                     <label>mail</label>
-                    <input type="mail" oninput="checkLen(\'mail\',255)"  placeholder="mail" id="mail">
+                    <input type="mail" oninput="checkLen('mail',255)"  placeholder="mail" id="mail">
                     <p id="limitmail">255/255</p><p id="erromail"></p>
                 </div>
             </div>
@@ -67,7 +67,7 @@
             <div class="col-md-4" style="background-color: white">
                 <div class="col-md-2 col-md-offset-5">
                     <label>téléphone</label>
-                    <input type="text" oninput="checkLen(\'tel\',10)" placeholder="telephon number" id="tel">
+                    <input type="text" oninput="checkLen('tel',10)" placeholder="telephon number" id="tel">
                     <p id="limittel">10/10</p><p id="errotel"></p>
                 </div>
             </div>
@@ -77,7 +77,7 @@
             <div class="col-md-4" style="background-color: white" >
                 <div class="col-md-2 col-md-offset-5">
                     <label>immatriculation</label>
-                    <input type="text" placeholder="imatriculation" oninput="checkLen(\'imatriculation\',50)" id="imatriculation">
+                    <input type="text" placeholder="imatriculation" oninput="checkLen('imatriculation',50)" id="imatriculation">
                     <p id="limitimatriculation">50/50</p><p id="erroimatriculation"></p>
                 </div>
                 <div class="col-md-2 col-md-offset-5">
