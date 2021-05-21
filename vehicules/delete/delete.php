aeca6f1be4355fd6ab49e458d58258fc835ebb37
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-21 14:19:26 +0200

    maj crud user

--- vehicules/delete/delete.php
+++ vehicules/delete/delete.php
@@ -7,10 +7,12 @@ if(isset($_GET)) {
     $sql = buildsDelete("vehicule", $idCheck);
     $params = array($idCheck);
     if (execRequestDelete($sql, $params)) {
+        header("Access-Control-Allow-Origin: *");
         header('Content-type: Application/json');
         echo json_encode(["success"=>1]);
     } else {
         http_response_code(400);
+        echo json_encode(["success"=>0]);
     }
 } else {
     //echo "error";
