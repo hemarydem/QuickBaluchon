be3a7072d14595289b35b14cc25626bb03bc103c
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-21 11:02:30 +0200

    correction script js inscription client succes()

--- signUp/scipt/driverScript.js
+++ signUp/scipt/driverScript.js
@@ -7,48 +7,13 @@
  * check if input are correctly filled to be send to the api
  */
 
-/*
-*
-* display the good inputs for the user
-*
-*/
-
-function displayForm(elementApp,urlLink) {
-    let request = new XMLHttpRequest();  
-    request.open("GET",urlLink,true); 
-    request.onreadystatechange = function() {
-        if(request.readyState == 4) {
-                if(request.status == 200) {
-                    console.log(request.responseText);
-                    typeof(request.responseText);
-                    app.innerHTML = request.responseText;
-            } else {
-                alert("Error: returned status code " + request.status + " " + request.statusText);
-            }
-        }
-    }
-    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    request.send();
-}
-
 let app = document.getElementById("app");
 
-/*
-*
-* Set up functions
-*
-*/
-
-const formSelector = document.getElementById("frmSelector");
-
-formSelector.addEventListener('change', (event) => {
-    let selectValue = formSelector.value;
-    selectValue = parseInt(selectValue, 10);
-    selectValue == 1 ? displayForm(app,"./driverForm.php"): displayForm(app,"./clientForm.php")
-});
-
 let webSitePath ="https://quickbaluchonservice.site";
-let homePage =[webSitePath + "/QuickBaluchon/home/homeDriver.php", webSitePath + "/QuickBaluchon/home/homeAdmin.php",webSitePath + "/QuickBaluchon/home/homeUser.php"];
+webSitePath = webSitePath + "/QuickBaluchon/home/homeDriver.php";
+
+
+//let homePage =[webSitePath + "/QuickBaluchon/home/homeDriver.php", webSitePath + "/QuickBaluchon/home/homeAdmin.php",];
 
 /*
  *  checkLen
@@ -58,7 +23,8 @@ let homePage =[webSitePath + "/QuickBaluchon/home/homeDriver.php", webSitePath +
  * check if the input don't have a to long string 
  * 
  * function is void
- * */
+ * principal purpose is to give a feed back to the user
+ */
 
 function checkLen(StrElementId,limit) {
     let charWarning = ""; 
@@ -76,55 +42,8 @@ function checkLen(StrElementId,limit) {
         warningElement.style.color = "red";
     }
 }
-/*
- *  validate
- *
- * check general of the inputs
- * if not to long
- * if there is appropriate chars
- * 
- * function is void
- * 
- * it call ajaxSendPost()
- * */
+//-------------------------------------------------------------//
 
-function validate() {
-    let canContainSpace = false;
-    let mustNotContainSpace = true;
-    let OnlyNumber = true;
-    let OnlyNumberNot = false;
-    let OnlyLetter = true;
-    let OnlyLetterNot = false;
-
-    let allowedSend = [true,true,true,true,true,true,true];
-    
-    allowedSend[0] = checkInput("name",50,OnlyNumberNot,OnlyLetter,canContainSpace);
-    
-    allowedSend [1]= checkInput("firstname",50,OnlyNumberNot,OnlyLetter,canContainSpace);
-    
-    allowedSend[2] = checkInput("pssword",255,OnlyNumberNot,OnlyLetterNot,mustNotContainSpace);
-
-    allowedSend[3] = checkInput("address",255,OnlyNumberNot,OnlyLetterNot,canContainSpace);
-
-    allowedSend[4] = checkInput("tel",10,OnlyNumber,OnlyLetterNot,mustNotContainSpace);
-
-    allowedSend[5] = checkInput("mail",255,OnlyNumberNot,OnlyLetterNot,mustNotContainSpace);
-
-    allowedSend[6] = checkInput("numSiret",50,OnlyNumberNot,OnlyLetterNot,mustNotContainSpace);
-
-    console.log(allowedSend);
-    let block = 0;
-    allowedSend.forEach(element => {            // check if each input was validate
-        if(element == false){
-            console.log("envoie pas");
-            block = 1;
-        }
-    });
-    if(block == 0) {
-        ajaxSendPost(getData(),"https://quickbaluchonservice.site/api/QuickBaluchon/users/post/creat.php");
-    }
-    console.log(" FIN");
-}
 /* checkInput
 *
 * arg 
@@ -179,6 +98,107 @@ function innerMessagetoElement(idInpuEl,strMessageError) {
 
 
 
+/*
+*get data
+*function do get data in the input
+*
+* no argument
+*
+* return array
+*/
+function  getData() {
+    let name = document.getElementById("name").value;
+    let firstname = document.getElementById("firstname").value;
+    let mail = document.getElementById("mail").value;//
+    let numSiret= document.getElementById("numSiret").value;
+    let address = document.getElementById("address").value;
+    let password= document.getElementById("pssword").value;//
+    let tel = document.getElementById("tel").value;//
+    let statut = 3;
+    name = name.trim();
+    firstname = firstname.trim();
+    mail = mail.trim();
+    numSiret= numSiret.trim();
+    address = address.trim();
+    password = password.trim();
+    tel = tel.trim();
+
+    mail = mail.replace(/\s/, ''); 
+    tel = tel.replace(/\s/, '');
+    if(numSiret.length != 0){
+        numSiret = numSiret.replace(/\s/, '');
+    } else {
+        numSiret = 0;
+    }
+    let array = [name,firstname,mail,address,numSiret,password,tel,statut];
+    return array;
+}
+
+
+
+/*
+ *  validate
+ *
+ * check general of the inputs
+ * if not to long
+ * if there is appropriate chars
+ * 
+ * function is void
+ * 
+ * it call ajaxSendPost()
+ * */
+
+function validate() {
+    let canContainSpace = false;
+    let mustNotContainSpace = true;
+    let OnlyNumber = true;
+    let OnlyNumberNot = false;
+    let OnlyLetter = true;
+    let OnlyLetterNot = false;
+
+    let allowedSend = [true,true,true,true,true,true,true,true,true,true,true];
+    
+    allowedSend[0] = checkInput("name",50,OnlyNumberNot,OnlyLetter,canContainSpace);
+    
+    allowedSend [1]= checkInput("firstname",50,OnlyNumberNot,OnlyLetter,canContainSpace);
+    
+    allowedSend[2] = checkInput("pssword",255,OnlyNumberNot,OnlyLetterNot,mustNotContainSpace);
+
+    allowedSend[3] = checkInput("address",255,OnlyNumberNot,OnlyLetterNot,canContainSpace);
+
+    allowedSend[4] = checkInput("tel",10,OnlyNumber,OnlyLetterNot,mustNotContainSpace);
+
+    allowedSend[5] = checkInput("mail",255,OnlyNumberNot,OnlyLetterNot,mustNotContainSpace);
+
+    allowedSend[6] = checkInput("numSiret",50,OnlyNumberNot,OnlyLetterNot,mustNotContainSpace);
+
+    //vehicule inputs
+
+    allowedSend[7] = checkInput("imatriculation",50,OnlyNumberNot,OnlyLetterNot,mustNotContainSpace);
+    
+    allowedSend[8]= checkInput("nbColis",50,OnlyNumber,OnlyLetterNot,mustNotContainSpace);
+    
+    allowedSend[9] = checkInput("volumeMax",50,OnlyNumber,OnlyLetterNot,mustNotContainSpace);
+
+    allowedSend[10] = checkInput("weightMax",50,OnlyNumber,OnlyLetterNot,mustNotContainSpace);
+
+
+    console.log(allowedSend);
+    let block = 0;
+    allowedSend.forEach(element => {            // check if each input was validate
+        if(element == false){
+            console.log("envoie pas");
+            block = 1;
+        }
+    });
+    if(block == 0) {
+        ajaxSendPost(getData(),"https://quickbaluchonservice.site/api/QuickBaluchon/users/post/creat.php");
+        success();
+    }
+    console.log(" FIN");
+}
+
+
 function ajaxSendPost(data,urlLink) {
         //driver 1
         //admin 2
@@ -202,13 +222,15 @@ function ajaxSendPost(data,urlLink) {
             if(request.readyState == 4) {
                     if(request.status == 200) {
                         let ObjJson = JSON.parse(request.responseText);
-                        console.log(ObjeJson);
-                        let num = ObjJson["statut"].toString();
-                        num = parseInt(num);
-                        num--;
-                        homePage[num];
-                        console.log(homePage[num]);
-                        window.location.href = homePage[ObjJson["statut"] - 1];
+                        let idRes = ObjJson["id"];
+                        let mailRes = ObjJson["mail"];
+                        sendMailConfirmation(idRes,mailRes);
+                        console.log(ObjJson);
+                        let div = document.createElement("div");
+                        app.appendChild(div);
+                        div.innerHTML = ObjJson["id"];
+                        div.setAttribute("id","di");
+                        div.hidden=true;
                 } else {
                     alert("Error: returned status code " + request.status + " " + request.statusText);
                 }
@@ -218,44 +240,169 @@ function ajaxSendPost(data,urlLink) {
         request.send(JSON.stringify(jsonToSend));
 }
 
-
-
-/*
-*get data
-*function do get data in the input
-*
-* no argument
-*
-* return array
-*/
-function  getData() {
-    let name = document.getElementById("name").value;
-    let firstname = document.getElementById("firstname").value;
-    let mail = document.getElementById("mail").value;//
-    let numSiret= document.getElementById("numSiret").value;
-    let address = document.getElementById("address").value;
-    let password= document.getElementById("pssword").value;//
-    let tel = document.getElementById("tel").value;//
-    let statut = document.getElementById("statut").value;
-    name = name.trim();
-    firstname = firstname.trim();
-    mail = mail.trim();
-    numSiret= numSiret.trim();
-    address = address.trim();
-    password = password.trim();
-    tel = tel.trim();
-
-    mail = mail.replace(/\s/, ''); 
-    tel = tel.replace(/\s/, '');
-    if(numSiret.length != 0){
-        numSiret = numSiret.replace(/\s/, '');
-    } else {
-        numSiret = 0;
+function sendMailConfirmation(resId,resMail) {
+    let jsonToSend = {
+        "id":resId,
+        "mail":resMail
+    };
+    let request = new XMLHttpRequest();  
+    request.open("POST",'https://quickbaluchonservice.site/QuickBaluchon/utls/mail.php',true); 
+    request.onreadystatechange = function() {
+        if(request.readyState == 4) {
+                if(request.status == 200) {
+                    let ObjJson = JSON.parse(request.responseText);
+                    console.log(ObjJson);
+                    if(ObjJson.hasOwnProperty("message")) {
+                        console.log("error mail not send");
+                    } else {
+                        setTokenOnUserprofile(resId,ObjJson["token"]);
+                    }
+            } else {
+                alert("Error: returned status code " + request.status + " " + request.statusText);
+            }
+        }
     }
-    let array = [name,firstname,mail,address,numSiret,password,tel,statut];
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    request.send(JSON.stringify(jsonToSend));
+}
+
+
+function setTokenOnUserprofile(resId,resToken) {
+    let jsonToSend = {
+        "id":resId,
+        "tokenEmail":resToken
+    };
+    let request = new XMLHttpRequest();  
+    request.open("POST","https://quickbaluchonservice.site/api/QuickBaluchon/users/post/update.php",true); 
+    request.onreadystatechange = function() {
+        if(request.readyState == 4) {
+                if(request.status == 200) {
+                    let ObjJson = JSON.parse(request.responseText);
+                    console.log(ObjJson);
+                    if(ObjJson.hasOwnProperty("message")) {
+                        console.log("error maj profil user");
+                    } else {
+                        addVehicule(getDataVehicule())
+                    }
+            } else {
+                alert("Error: returned status code " + request.status + " " + request.statusText);
+            }
+        }
+    }
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    request.send(JSON.stringify(jsonToSend));
+}
+
+
+function success() {
+    app.innerHTML= "";
+    let h1 = document.createElement("h1");
+    let h2 = document.createElement("h2");
+    let h3 = document.createElement("h3");
+    let div = document.createElement("div");
+    div.setAttribute("class", "row");
+   
+    h1.innerHTML = "FÉLICITATION VOUS AVEZ FINI DE COMPLETER LE FORMULAIRE";
+    h2.innerHTML = "Vous devez avoir reçu un mail pour activer votre compte";
+    h3.innerHTML = "Si vous ne le trouvez pas vérifier dans vos indésirable";
+    h1.setAttribute("class", "center-block");
+    h2.setAttribute("class", "center-block");
+    h3.setAttribute("class", "center-block");
+    
+    app.appendChild(div);
+    div.appendChild(h1);
+    div.appendChild(h2);
+    div.appendChild(h3);
+
+    h1.style = "float:none;"
+    h2.style = "float:none;"
+    h3.style = "float:none;"
+
+    h1.style.color = "green";
+    h2.style.color = "green";
+    h3.style.color = "green";
+}
+
+
+// add vehicule
+
+function addVehicule(arrayDataVehicule) {
+    let jsonToSend = {
+        "immatriculation":arrayDataVehicule[0],
+        "nbColis":arrayDataVehicule[1],
+        "volumeMax":arrayDataVehicule[2],
+        "weightMax":arrayDataVehicule[3]
+    };
+    let request = new XMLHttpRequest();  
+    request.open("POST","https://quickbaluchonservice.site/api/QuickBaluchon/vehicules/post/creat.php",true); 
+    request.onreadystatechange = function() {
+        if(request.readyState == 4) {
+                if(request.status == 200) {
+                    let ObjJson = JSON.parse(request.responseText);
+                    console.log(ObjJson);
+                    if(ObjJson.hasOwnProperty("message")) {
+                        console.log("error add vehicule");
+                        console.log(ObjJson["message"]);
+                    } else {
+                        console.log(ObjJson);
+                        let div = document.createElement("div");
+                        app.appendChild(div);
+                        div.innerHTML = ObjJson["id"];
+                        div.setAttribute("id","diVehicule");
+                        div.hidden=true;
+                    }
+            } else {
+                alert("Error: returned status code " + request.status + " " + request.statusText);
+            }
+        }
+    }
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    request.send(JSON.stringify(jsonToSend));
+}
+
+function  getDataVehicule() {
+    let immatriculation = document.getElementById("imatriculation").value;
+    let nbColis = document.getElementById("nbColis").value;
+    let volumeMax = document.getElementById("volumeMax").value;//
+    let weightMax= document.getElementById("weightMax").value;
+    immatriculation = immatriculation.trim();
+    nbColis = nbColis.trim();
+    volumeMax = volumeMax.trim();
+    weightMax= weightMax.trim();
+
+    immatriculation = immatriculation.replace(/\s/, ''); 
+    nbColis = nbColis.replace(/\s/, '');
+    volumeMax = volumeMax.replace(/\s/, ''); 
+    weightMax = weightMax.replace(/\s/, '');
+
+    let array = [immatriculation,nbColis,volumeMax,weightMax];
     return array;
 }
 
 
-
-
+function addOwn() {
+    let idU = String(document.getElementById("di").innerHTML);
+    let idVeh = String(document.getElementById("diVehicule").innerHTML);
+    let jsonToSend = {
+        "idVehicule":idU,
+        "idUser":idVeh
+    };
+    let request = new XMLHttpRequest();  
+    request.open("POST","https://quickbaluchonservice.site/api/QuickBaluchon/owns/post/creat.php",true); 
+    request.onreadystatechange = function() {
+        if(request.readyState == 4) {
+                if(request.status == 200) {
+                    let ObjJson = JSON.parse(request.responseText);
+                    console.log(ObjJson);
+                    if(ObjJson.hasOwnProperty("message")) {
+                        console.log("error add vehicule");
+                        console.log(ObjJson["message"]);
+                    } 
+            } else {
+                alert("Error: returned status code " + request.status + " " + request.statusText);
+            }
+        }
+    }
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    request.send(JSON.stringify(jsonToSend));
+}
