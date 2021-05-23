55f86239a2fabba3cae95690dad359ab347c7ba6
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-23 10:55:52 +0200

    bug connection fixed 2

--- home/homeDriver.php
+++ home/homeDriver.php
@@ -34,7 +34,25 @@
             </div>
         </div>
         <div class="row justify-content-lg-center ">
-            hello
+            <form>
+                <div class="form-group">
+                    <label for="formGroupExampleInput">Example label</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Another label</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Another label</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Another label</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                </div>
+                <button type="submit" class="btn btn-primary">Sign in</button>
+            </form>
         </div>
     </div>
         
