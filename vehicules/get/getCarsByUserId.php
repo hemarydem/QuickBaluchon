5bfde650224657bb24b018bf8b86d7aa9196651c
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-23 19:20:43 +0200

    maj crud user

--- vehicules/get/getCarsByUserId.php
+++ vehicules/get/getCarsByUserId.php
@@ -9,8 +9,13 @@ if(isset($_GET)) {
     } else {
         erroJsonMssg("error id is missing");
     }
-    $sql = "SELECT VEHICULE.id, VEHICULE.imatriculation, VEHICULE.nbColis, VEHICULE.volumeMax, VEHICULE.weightMax, VEHICULE.employ, VEHICULE.active FROM VEHICULE INNER JOIN OWN ON OWN.idVehicule=VEHICULE.id AND OWN.idUser=? AND VEHICULE.active=?";
-    $rows = execRequestGetALLResults( $sql, [$id]);
+    if(isset($_GET["active"])) {
+        $active = intval($_GET["active"]);
+    } else {
+        erroJsonMssg("error id is missing");
+    }
+    $sql = "SELECT VEHICULE.id, VEHICULE.imatriculation, VEHICULE.nbColis, VEHICULE.volumeMax, VEHICULE.weightMax, VEHICULE.employ, VEHICULE.active FROM VEHICULE AND VEHICULE.active=? INNER JOIN OWN ON OWN.idVehicule=VEHICULE.id AND OWN.idUser=?";
+    $rows = execRequestGetALLResults( $sql, [$active,$id]);
     $json = json_encode($rows);
     header("Content-Type: application/json");
     print_r($json);
