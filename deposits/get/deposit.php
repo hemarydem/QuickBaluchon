dce116a8433300ca664147e7448fe984a6586a24
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-25 10:36:28 +0200

    maj function get on mix primaray key

--- deposits/get/deposit.php
+++ deposits/get/deposit.php
@@ -8,6 +8,11 @@ if(isset($_GET)) {
     $sql = buildsSelectAndattributsForMixePrimaryKey($_GET, "DEPOSIT");
     $params = buildParamsForMixePrimaryKey($_GET);
     $rows = dataBaseFindOneForMixePrimaryKey($sql,$params); //db.php
+    if($rows == null){
+        header("Content-Type: application/json");
+        echo json_encode(["message"=> "DEPOSIT not found"]);
+        exit(1)
+    }
     $json = json_encode($rows);
     header("Content-Type: application/json");
     print_r($json);
