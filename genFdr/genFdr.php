b3e3927f5bef04979bca59fecc702dba9f8508c5
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-26 21:31:55 +0200

    feature list depot

--- genFdr/genFdr.php
+++ genFdr/genFdr.php
@@ -1,5 +1,5 @@
 <?php
-/*$content = file_get_contents('php://input');
+$content = file_get_contents('php://input');
 $data = json_decode($content, true);
 foreach ($data as $key => $value ){
     $data[$key]["longitude"] = 0;
@@ -14,24 +14,14 @@ foreach ($data as $key => $value ){
     $cURLConnection = curl_init();
     curl_setopt($cURLConnection, CURLOPT_URL, $urlBase);
     curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
-    $result = curl_exec($cURLConnection);
+    $result = json_decode(curl_exec($cURLConnection), true);
     curl_close($cURLConnection);
-    //header('Content-type: Application/json');
-    $result["features"][0]["geometry"]["coordinates"][0];
-    $result["features"][0]["geometry"]["coordinates"][1];
-    //$data[$key]["longitude"] = 0;
-   // $data[$key]["latitude"] = 0;
+    $data[$key]["longitude"] = $result["features"][0]["geometry"]["coordinates"][0];
+    $data[$key]["latitude"] =  $result["features"][0]["geometry"]["coordinates"][1];
 }
-*/
-header('Content-type: Application/json');
-$urlBase = "https://api-adresse.data.gouv.fr/search/?q=43+chemin+de+la+source+78590";
-//echo $urlBase;
-$cURLConnection = curl_init();
-curl_setopt($cURLConnection, CURLOPT_URL, $urlBase);
-curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
-$result = json_decode(curl_exec($cURLConnection), true);
-curl_close($cURLConnection);
-print_r($result["features"][0]["geometry"]["coordinates"]);
+
+
+print_r($data);
 
 
 
