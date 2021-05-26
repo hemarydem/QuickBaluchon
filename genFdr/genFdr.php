cc7b36252398ce67571c33d7aec7e85cb5f7ee12
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-26 21:17:12 +0200

    feature list depot

--- genFdr/genFdr.php
+++ genFdr/genFdr.php
@@ -1,5 +1,5 @@
 <?php
-$content = file_get_contents('php://input');
+/*$content = file_get_contents('php://input');
 $data = json_decode($content, true);
 foreach ($data as $key => $value ){
     $data[$key]["longitude"] = 0;
@@ -22,4 +22,12 @@ foreach ($data as $key => $value ){
     //$data[$key]["longitude"] = 0;
    // $data[$key]["latitude"] = 0;
 }
+*/
 
+$urlBase = "https://api-adresse.data.gouv.fr/search/?q=43+chemin+de+la+source+78590";
+echo $urlBase;
+$cURLConnection = curl_init();
+curl_setopt($cURLConnection, CURLOPT_URL, $urlBase);
+curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($cURLConnection);
+curl_close($cURLConnection);
