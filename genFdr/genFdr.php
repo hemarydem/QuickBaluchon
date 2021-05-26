9a3da9293f0b24c4fa478d5e8b25547e2c074c19
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-26 21:52:42 +0200

    feature list depot

--- genFdr/genFdr.php
+++ genFdr/genFdr.php
@@ -1,27 +1,53 @@
 <?php
-$content = file_get_contents('php://input');
-$data = json_decode($content, true);
-foreach ($data as $key => $value ){
-    $data[$key]["longitude"] = 0;
-    $data[$key]["latitude"] = 0;
-}
+if(isset($_GET)){
 
-foreach ($data as $key => $value ){
-    $address = str_replace('\'', '',  $data[$key]["adresse"]);
-    $address = str_replace(' ', '+',  $address);
-    $urlBase = "https://api-adresse.data.gouv.fr/search/?q=".$address."+".$data[$key]["codePostale"];
-    echo $urlBase;
+
+    $urlBase = "https://quickbaluchonservice.site/api/QuickBaluchon/colis/get/list.php?limit=1000&offset=0&isPayed=1&sendingStatut=1&id&adresse&codePostale&dDate=" . $_GET["date"] . "&idDepot=". $_GET["idDepot"];
     $cURLConnection = curl_init();
     curl_setopt($cURLConnection, CURLOPT_URL, $urlBase);
     curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
-    $result = json_decode(curl_exec($cURLConnection), true);
+    $data = json_decode(curl_exec($cURLConnection), true);
     curl_close($cURLConnection);
-    $data[$key]["longitude"] = $result["features"][0]["geometry"]["coordinates"][0];
-    $data[$key]["latitude"] =  $result["features"][0]["geometry"]["coordinates"][1];
+
+    foreach ($data as $key => $value ){
+        $data[$key]["longitude"] = 0;
+        $data[$key]["latitude"] = 0;
+        $data[$key]["gap"] = 0;
+    }
+    print_r($data);
+    /*
+    foreach ($data as $key => $value ){
+        $address = str_replace('\'', '',  $data[$key]["adresse"]);
+        $address = str_replace(' ', '+',  $address);
+        $urlBase = "https://api-adresse.data.gouv.fr/search/?q=".$address."+".$data[$key]["codePostale"];
+        $cURLConnection = curl_init();
+        curl_setopt($cURLConnection, CURLOPT_URL, $urlBase);
+        curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+        $result = json_decode(curl_exec($cURLConnection), true);
+        curl_close($cURLConnection);
+        $data[$key]["longitude"] = $result["features"][0]["geometry"]["coordinates"][0];
+        $data[$key]["latitude"] =  $result["features"][0]["geometry"]["coordinates"][1];
+    }
+
+
+    foreach ($data as $key => $value ) {
+        $urlp1 = "https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?";
+        $stratPoint = "origins=" . $data[$key]["longitude"] . ",". $data[$key]["latitude"];
+        $endPoint ="&destinations=" . $data[$key]["longitude"] . "," . $data[$key]["latitude"];
+        $urlp2End = "&travelMode=driving&key=AvodcS2fiYqi1KDA7R1XZ-FQV2qEJKihfcFKfcpQrZwdWRCMLXDJ67WrQwRthFe8";
+        $urlBase = "https://api-adresse.data.gouv.fr/search/?q=".$address."+".$data[$key]["codePostale"];
+        echo $urlBase;
+        $cURLConnection = curl_init();
+        curl_setopt($cURLConnection, CURLOPT_URL, $urlBase);
+        curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+        $result = json_decode(curl_exec($cURLConnection), true);
+        curl_close($cURLConnection);
+        $data[$key]["longitude"] = $result["features"][0]["geometry"]["coordinates"][0];
+        $data[$key]["latitude"] =  $result["features"][0]["geometry"]["coordinates"][1];
+    }
+    */
+
 }
-
-
-print_r($data);
 
 
 
