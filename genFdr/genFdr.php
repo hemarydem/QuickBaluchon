87372392720d8210cb60e8d7ab535d1e36d321a9
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-26 22:25:54 +0200

    feature list depot

--- genFdr/genFdr.php
+++ genFdr/genFdr.php
@@ -1,6 +1,5 @@
 <?php
 if(isset($_GET)){
-/*
     $urlBase = "https://quickbaluchonservice.site/api/QuickBaluchon/colis/get/list.php?limit=1000&offset=0&isPayed=1&sendingStatut=1&id&adresse&codePostale&dDate=" . $_GET["date"] . "&idDepot=". $_GET["idDepot"];
     $cURLConnection = curl_init();
     curl_setopt($cURLConnection, CURLOPT_URL, $urlBase);
@@ -49,16 +48,9 @@ if(isset($_GET)){
         curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
         $result = json_decode(curl_exec($cURLConnection), true);
         curl_close($cURLConnection);
-        $data[$key]["gap"] =  $result["features"][0]["geometry"]["coordinates"][1];
-    }*/
-    $urlBase ="https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins=48.892880582429321,2.1062883883451931&destinations=48.8480178,2.0556039&travelMode=driving&key=AvodcS2fiYqi1KDA7R1XZ-FQV2qEJKihfcFKfcpQrZwdWRCMLXDJ67WrQwRthFe8";
-    $cURLConnection = curl_init();
-    curl_setopt($cURLConnection, CURLOPT_URL, $urlBase);
-    curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
-    $result = json_decode(curl_exec($cURLConnection), true);
-    curl_close($cURLConnection);
-    echo $result["resourceSets"][0]["resources"][0]["results"][0]["travelDistance"];
-   // print_r($result);
+        $data[$key]["gap"] =  $result["resourceSets"][0]["resources"][0]["results"][0]["travelDistance"];
+    }
+    print_r($data);
 }
 
 
