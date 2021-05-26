620194309ea9d5def02c28da5c4be0c7c3c7080e
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-26 20:45:36 +0200

    feature list depot

--- genFdr/genFdr.php
+++ genFdr/genFdr.php
@@ -1,5 +1,12 @@
 <?php
 
+if(isset($_POST)){
+    print_r($_POST);
+
+}
+
+
+/*
 $urlBase = "https://api-adresse.data.gouv.fr/search/?q=43+chemin+de+la+source+78590";
 $cURLConnection = curl_init();
 curl_setopt($cURLConnection, CURLOPT_URL, $urlBase);
@@ -8,3 +15,4 @@ $result = curl_exec($cURLConnection);
 curl_close($cURLConnection);
 header('Content-type: Application/json');
 print_r($result);
+*/
