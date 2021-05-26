f8643d5061f22463edb9657817f7ad631d304040
hemarydem <43240309+hemarydem@users.noreply.github.com>
2021-05-26 23:56:52 +0200

    feature list depot

--- genFdr/genFdr.php
+++ genFdr/genFdr.php
@@ -87,14 +87,14 @@ if(isset($_GET)){
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $dataDelyvery = json_decode(curl_exec($ch),true);
     curl_close($ch);
-    echo
+
     print_r($dataDelyvery);
 
     $strTocreatDelyveryCheck = [
         "idDelivery"=> $dataDelyvery["id"],
         "idUser"=> $_GET["id"]
     ];
-/*
+
     $urlBase = "https://quickbaluchonservice.site/api/QuickBaluchon/checkDeliverys/post/creat.php";
     $ch = curl_init($urlBase);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $strTocreatDelyveryCheck);
@@ -104,7 +104,7 @@ if(isset($_GET)){
     curl_close($ch);
     print_r($result);
 
-*/
+
 /*
     $min = 0;
 
